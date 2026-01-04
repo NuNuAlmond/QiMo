7fab9066e656af23c1405d24b1b5590ecb5b55fc
NuNuAlmond <1547542903@qq.com>
2026-01-04 16:11:15 +0800

    整体修改（3）

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
 import cv2
 import matplotlib.pyplot as plt #导入matplotlib的绘图接口，用于弹出窗口显示图片/曲线
 
-def show_image(title, image, is_gray=False): #在一个窗口中显示图像
-    plt.figure(figsize=(6, 6)) #新建一个绘图窗口figure
-    if is_gray: #如果is_gray=True：按灰度图显示
+def show_image(title, image, is_gray=False): #在一个窗口中显示图像，是否按灰度图显示
+    plt.figure(figsize=(6, 6)) #新建一个matplotlib绘图窗口figure
+    if is_gray: #如果is_gray=True，按灰度图显示
         plt.imshow(image, cmap='gray')
     else:
         plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     plt.title(title)
-    plt.axis('off') #关闭坐标轴刻度和边框
-    plt.show()
+    plt.axis('off') #关闭x/y坐标轴刻度和边框
+    plt.show() #弹出窗口并显示
 
 def show_histogram(hist, title="Histogram"): #显示直方图/曲线
     plt.figure()
